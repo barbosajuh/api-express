d8bd3e6f2837fa8dadfb8214f99a1a80f0648a4f
Julia <julia.magalhaes@estudante.ifms.edu.br>
2026-08-31 21:24:11 +0000

    segundo comit

--- app.js
+++ app.js
@@ -1,14 +1,39 @@
 import express from 'express'
+import res from 'express/lib/response';
 
 const app = express()
 const PORT = 3000
 
+const usuarios = [
+    {id: 1, nome: 'Gabriel'},
+    {id: 2, nome: 'João'},
+      {id: 3, nome: 'Paulo'}
+]
+
 app.get('/', (req, res) => {
-    res.send('Bem-vindo ao express!');
+    res.send('Bem-vindo ao express!')
 });
 
-app.listen(PORT, () => {
-    console.log('Servidor rodando em http://loca1host: ${PORT}'
-        
-    )
+app.get('/usuarios', (req, res) => {
+    res.json(usuarios);
 })
+
+app.post('/usuario', () => {
+    const nomeUsuario = {
+        id: usuarios.length +1,
+        nome: 'Lucas'
+    }
+    usuarios.push(novoUsuario);
+    res.status(201).json(novoUsuario);
+})
+app.get('/usuarios', (req, res) =>{
+    const id = req.params.id;
+    const usuario = usuarios.find(u => u.id === parseInt(id));
+    if (!usuario){
+        return res.status(404).json({error: "Usuario não encontrado!"});
+    }
+    res.json(usuario);
+});
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT} `)
+});
